93aa5f819b468bd4c89fabed1ad5d646592e9798
Mikel Goig <m@mikelgoig.com>
2024-12-20 13:06:21 +0000

    refactor: move Kernel to shared infrastructure

---
+++ service/src/Shared/Infrastructure/Symfony/Kernel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Shared\Infrastructure\Symfony;
+
+use App\Shared\Application\Bus\Command\AsCommandHandler;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\ChildDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+
+    protected function build(ContainerBuilder $container): void
+    {
+        $container->registerAttributeForAutoconfiguration(AsCommandHandler::class, static function (ChildDefinition $definition): void {
+            $definition->addTag('messenger.message_handler', ['bus' => 'command.bus']);
+        });
+    }
+}
