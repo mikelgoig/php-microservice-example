ee52561c1033f1511f45f7cb5b004a5a4d6abebc
Mikel Goig <m@mikelgoig.com>
2025-01-30 19:53:11 +0100

    feat: add Prooph PDO event store configuration and tables

---
+++ service/src/Shared/Infrastructure/Ecotone/EcotoneConfiguration.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Infrastructure\Ecotone;
+
+use Ecotone\Messaging\Attribute\ServiceContext;
+use Ecotone\SymfonyBundle\Config\SymfonyConnectionReference;
+
+final readonly class EcotoneConfiguration
+{
+    #[ServiceContext]
+    public function dbalConfiguration(): SymfonyConnectionReference
+    {
+        $connection = SymfonyConnectionReference::defaultConnection('default');
+        \assert($connection instanceof SymfonyConnectionReference);
+        return $connection;
+    }
+}
